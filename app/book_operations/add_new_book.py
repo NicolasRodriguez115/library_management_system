be03fa74f15db2856f42fb68b8fd67afaa44d406
NicolasRodriguez115 <nr115935@gmail.com>
2024-07-02 17:32:59 -0400

    You can add a new author when adding a new book. Other fixes

--- app/book_operations/add_new_book.py
+++ app/book_operations/add_new_book.py
@@ -1,5 +1,5 @@
 from app.classes.book import Book
-from app.classes.author import authors
+from app.classes.author import authors, Author
 def new_book():
     title = input("Enter the title of the book:\n").title()
     author_name = input("Enter the name of the author for the book:\n").title()
@@ -9,7 +9,13 @@ def new_book():
     for author in authors:
         if author.name == author_name:
             author.add_book(title)
-
+            input(f"You've succesfully added {title} by {author_name} to the library! Press 'enter' to go back.\n ")
+            return
+        
+    print("It appears that this author isn't registered.\nPlease enter the author information so we can add it to the system.")
+    nationality = input("What's the nationality of the author?").capitalize()
+    new_author = Author(author_name, nationality)
+    new_author.new_author()
+    new_author.add_book(title)
     input(f"You've succesfully added {title} by {author_name} to the library! Press 'enter' to go back.\n ")
-    
     return
--- app/user_operations/view_user_details.py
+++ app/user_operations/view_user_details.py
@@ -1,15 +1,12 @@
 from app.classes.user import users
 def user_details():
     select_user = input("Enter the ID of the user to see their information\n")
+    
     for user in users:
-        print(type(user.user_id))
-        print((user.user_id))
-        print(select_user == user.user_id)
-        input("")
-        # if user.user_id == select_user:
-        #     user.user_details()
-        #     input("Press 'enter' to go back.\n ")
-        #     return
-        # else:
-        #     input("That isn't an ID associated to a current user. You'll return to the main menu after pressing 'enter'\n ")
-        #     return
+        if user.user_id == select_user:
+            user.user_details()
+            input("Press 'enter' to go back.\n ")
+            return
+        
+    input("That isn't an ID associated to a current user. You'll return to the main menu after pressing 'enter'\n ")
+    return
